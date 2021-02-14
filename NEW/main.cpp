5716d02bcb6b42abe1f7088894db6e9b725166b0
JakubKosakowski <67673985+JakubKosakowski@users.noreply.github.com>
2021-02-14 01:41:13 +0100

    Create main.cpp

---
+++ NEW/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include "Game.h"
+
+int main()
+{
+    Game game;
+
+    while (game.running()) {
+        game.update();
+
+        game.render();
+    }
+
+    return 0;
+}
